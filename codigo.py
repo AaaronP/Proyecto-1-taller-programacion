9cf806ec8551dbe970ac2fb797462b528e4cc63a
Aaron <aaronperezwindows10@gmail.com>
2024-04-06 13:59:33 -0600

    ...

--- codigo.py
+++ codigo.py
@@ -1,8 +1,4 @@
-from eulerlib import Divisors
-
 import time
-
-inicio = time.time()
 
 # Referencias
 """
@@ -10,7 +6,6 @@ https://www.baeldung.com/cs/backtracking-algorithms#:~:text=Backtracking%20is%20
 https://www.w3schools.com/python/python_ref_list.asp
 https://www.w3schools.com/python/ref_string_format.asp
 https://ellibrodepython.com/tiempo-ejecucion-python
-https://pypi.org/project/eulerlib/
 https://docs.hektorprofe.net/python/funcionalidades-avanzadas/comprension-de-listas/
 """
 
@@ -18,17 +13,10 @@ https://docs.hektorprofe.net/python/funcionalidades-avanzadas/comprension-de-lis
 # Detectar si un numero sobrepasa el
 # limite de R
 def todo(n: int, r: int) -> bool:
-    list = []
-
-    while n != 0:
-        list.append(n % 10)
-        n //= 10
+    list = [int(i) for i in str(n)]
 
     if len(list) < 4:
         return False
-
-    # if len(list) == 4 and list.count(0) == 1:
-    #     return False
 
     if len(list) == 4:
         list.append(0)
@@ -40,33 +28,55 @@ def todo(n: int, r: int) -> bool:
     return True
 
 
-def filtrar(lista: list) -> list:
-    return [n for n in lista if n >= 1000]
+def todo2(n1, n2, r):
+    list1 = [int(i) for i in str(n1)]
+    list2 = [int(i) for i in str(n2)]
+
+    if len(list1) < 4 or len(list2) < 4:
+        return False
+
+    if len(list1) == 4:
+        list1.append(0)
+    if len(list2) == 4:
+        list2.append(0)
+
+    list = list1 + list2
+
+    for i in list:
+        if list.count(i) > r:
+            return False
+
+    return True
 
 
-def foo(c: int, r: int, t: int) -> None:
+def foo(c: int, r: int) -> None:
     if r > 5 or r < 1:
         print(-1)
         return
-    if t < 1 or t > 100:
-        print(-1)
-        return
 
-    i = 10000
-    a = 0
+    b = 9999
 
-    div = Divisors(99999)
-
-    while a < t:
-        for j in filtrar(div.divisors(i)):
-            if todo(i, r) and todo(j, r):
-                if i / j == c:
-                    print(f"{i}/{j}={c}")
-                    a += 1
-        i += 1
+    while b <= 99999:
+        if todo(b, r):
+            k = b // c
+            if not b % c and todo2(b, k, r):
+                print(f"{b}/{k}={c}")
+        b += 1
 
 
-foo(2, 1, 2)
+# cantidad de casos de prueba
+t = int(input())
+
+list = []
+for i in range(t):
+    string = input()
+    c, r = string.split()
+    list.append((int(c), int(r)))
+
+inicio = time.time()
+
+for i in list:
+    foo(i[0], i[1])
 
 fin = time.time()
 print("Seconds:", fin - inicio)
--- vjudge.py
+++ vjudge.py
@@ -1,24 +1,49 @@
-from math import factorial
+def exp(n,b):
+    if b == 0:
+        return 1
+    res = exp(n, b//2)
+    res *= res
+    if n % 2 == 0:
+        return res
+    return res * n
 
-def lcm(a, b):
-    list_a = []
-    list_b = []
+def convertR(n, b, r, m, e):
+    if n < r:
+        return m + n * b**e
+    return convertR(n//r, b, r, m + (n % r) * b**e, e + 1)
 
-    for i in range(1, a + 1):
-        list_a.append(a * i)
-    for i in range(1, b + 1):
-        list_b.append(b * i)
+def conversor(n, b, r):
+    m = 0
+    e = 0
 
-    print(list_a, list_b)
+    while n >= r:
+        m += (n % r) * b**e
+        e += 1
+        n = n // r
+    m += n * b**e
 
-lcm(4, 6)
+    return m
 
-def c3(a,b):
-    fact = factorial(a)
+def hola(n):
+    prod = 1
 
-    for i in range(1, fact + 1):
-        print(i)
+    for i in range(1, n+1):
+        suma = 0
+        for j in range(i, i*i+1):
+            suma += exp(j,i)
 
-    return fact
+        prod *= suma
+        suma = 0
+    
+    return prod
 
-print(c3(10,2))
+def suma(i, j, res = 0):
+    if j > i*i:
+        return res
+    return suma(i, j + 1, res + exp(j,i))
+
+def pi(n, i = 1, res = 1):
+    if i > n:
+        return res
+    return pi(n, i+1, res * suma(i, i, 0))
+
