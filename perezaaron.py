655a9800fe5f0df2bea43d714b737c7e640ee093
Aaron <aaronperezwindows10@gmail.com>
2024-04-12 22:40:48 -0600

    ...

--- perezaaron.py
+++ perezaaron.py
@@ -1,40 +1,3 @@
-# Detectar si un numero sobrepasa el
-# limite de r
-def todo(n1, n2, r):
-    # añadiendo un 0 al numero si solo tiene 4 digitos
-    if n2 >= 1000 and n2 < 10000:
-        n2 *= 10
-    list = str(n1) + str(n2)
-    for i in list:
-        if list.count(i) > r:
-            return False
-
-    return True
-
-
-def foo(c: int, r: int) -> None:
-    list = []
-    b = 10000
-    while b < 100000:
-        if b % c:
-            b += c
-            continue
-        k = b // c
-        if k < 1000:
-            b += c
-            continue
-
-        if todo(b, k, r):
-            list.append(f"{b}/{k}={c}\n")
-
-        b += c
-
-    return "".join(list)
-
-
-import time
-
-# cantidad de casos de prueba
 t = int(input())
 
 cases = []
@@ -43,6 +6,50 @@ for i in range(t):
     c, r = string.split()
     cases.append((int(c), int(r)))
 
+"""
+Dominio: n1, n2 y r, números naturales, donde n1 y n2 son los números a comprobar y r es el límite de comcurrencias de los dígitos
+Ambito: True, False
+"""
+
+
+def todo(n1, n2, r):
+    if n1 >= 1000 and n1 < 10000:
+        n1 *= 10
+    if n2 >= 1000 and n2 < 10000:
+        n2 *= 10
+
+    list = str(n1) + str(n2)
+    for i in list:
+        if list.count(i) > r:
+            return False
+
+    return True
+
+
+"""
+Dominio: c, r ambos números naturales, donde c es el número a encontrar y r es el limite de de concurrencias de los digitos
+Ambito: Devuelve una cadena de texto, ejemplo: 13485/2697=5\n13845/2769=5
+"""
+
+
+def foo(c: int, r: int) -> None:
+    string = ""
+    a = 1000
+
+    while a * c < 100000:
+        b = a * c
+        if todo(b, a, r):
+            string += f"{b}/{a}={c}\n"
+
+        a += 1
+
+    return string
+
+
+import time
+
+inicio = time.time()
+
 tests = []
 for i in cases:
     tests.append(foo(i[0], i[1]))
@@ -50,3 +57,6 @@ for i in cases:
 for i in tests:
     with open("output.txt", "a") as f:
         f.write(i + "\n")
+
+fin = time.time()
+print(fin - inicio)
