e3ceeea6de0e25c1bca6e8bc8b89854d333bea91
Aaron <aaronperezwindows10@gmail.com>
2024-04-13 13:52:11 -0600

    ...

--- perezaaron.py
+++ perezaaron.py
@@ -1,5 +1,6 @@
 t = int(input())
 
+# O(t)
 cases = []
 for i in range(t):
     string = input()
@@ -12,32 +13,39 @@ Ambito: True, False
 """
 
 
-def todo(n1, n2, r):
+def todo(n1: int, n2: int, r: int) -> bool:
     if n1 >= 1000 and n1 < 10000:
         n1 *= 10
     if n2 >= 1000 and n2 < 10000:
         n2 *= 10
 
+    # len(list) == 10
     list = str(n1) + str(n2)
+    # O(list^2) o O(10^2)
     for i in list:
         if list.count(i) > r:
             return False
-
     return True
 
 
 """
-Dominio: c, r ambos números naturales, donde c es el número a encontrar y r es el limite de de concurrencias de los digitos
+Dominio: c, r ambos números naturales, donde c es el número a encontrar y r es el limite de concurrencias de los digitos
 Ambito: Devuelve una cadena de texto, ejemplo: 13485/2697=5\n13845/2769=5
 """
 
 
-def foo(c: int, r: int) -> None:
+def foo(c: int, r: int) -> str:
     string = ""
     a = 1000
+    # en el peor de los casos haria 98999 bucles
+    # y en el mejor haria solo 4000 bucles
 
+    # 98999 * 100 = 9,899,900
+    # O(9 899 900)
     while a * c < 100000:
         b = a * c
+
+        # O(100) 100 bucles
         if todo(b, a, r):
             string += f"{b}/{a}={c}\n"
 
@@ -46,15 +54,11 @@ def foo(c: int, r: int) -> None:
     return string
 
 
-import time
-
-inicio = time.time()
+# O(t)
 tests = []
 for i in cases:
     tests.append(foo(i[0], i[1]))
 
-with open("output.txt", "a") as f:
-    for i in tests:
-        f.write(i + "\n")
-fin = time.time()
-print(fin - inicio)
+# O(t)
+for i in tests:
+    print(i)
